d6e8377e5f3f0f9a9a06287aef9d4b71beca5129
Berny Cantos <be@rny.cc>
2022-11-01 02:26:17 +0100

    +theater: extract cue event to its own scene

--- modules/system/emitter/emitter.gd
+++ modules/system/emitter/emitter.gd
@@ -5,3 +5,7 @@ export(Resource) var _signal = ResourceSignal.new()
 
 func emit(value = "") -> void:
 	_signal.emit_signal("signaled", value)
+
+
+func listen(object: Object, method: String, binds := [], flags := 0) -> int:
+	return _signal.connect("signaled", object, method, binds, flags)
