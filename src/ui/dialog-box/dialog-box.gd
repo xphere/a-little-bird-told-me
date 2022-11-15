2f73dd65f10368490f2b26f3f758801f1f3d51b9
Berny Cantos <be@rny.cc>
2022-11-15 23:10:20 +0100

    +dialog: notify dialog skip

--- src/ui/dialog-box/dialog-box.gd
+++ src/ui/dialog-box/dialog-box.gd
@@ -3,8 +3,8 @@ extends CanvasItem
 signal completed()
 
 
-onready var _content := $"Content/Text/Label" as Label
-onready var _speaker := $"Speaker/Text/Label" as Label
+onready var _content := $"%Content/Text/Label" as Label
+onready var _speaker := $"%Speaker/Text/Label" as Label
 
 
 var _characters_shown : float
@@ -14,10 +14,11 @@ var _characters_per_sec : float
 func info(content: String) -> void:
 	visible = true
 	_speaker.text = ""
-	$Speaker.visible = false
+	$"%Speaker".visible = false
+	$"%Continue".visible = false
 	_content.text = content
 	_content.visible_characters = -1
-	$Content.theme_type_variation = "PanelContainerInformation"
+	$"%Content".theme_type_variation = "PanelContainerInformation"
 
 
 func dialog(content: String, speaker: String, characters_per_sec: float) -> void:
@@ -25,21 +26,39 @@ func dialog(content: String, speaker: String, characters_per_sec: float) -> void
 	_characters_shown = 0
 	_characters_per_sec = characters_per_sec
 	_speaker.text = speaker
-	$Speaker.visible = true
+	$"%Speaker".visible = true
+	$"%Continue".visible = false
 	_content.text = content
 	_content.visible_characters = 0
-	$Content.theme_type_variation = ""
+	$"%Content".theme_type_variation = ""
 	set_process(true)
+	set_process_input(true)
 	yield(self, "completed")
-	yield(get_tree().create_timer(1.0), "timeout")
 	visible = false
 
 
+func _is_fully_shown() -> bool:
+	return _content.percent_visible >= 1.0
+
+
 func _process(delta: float) -> void:
-	if _content.percent_visible < 1.0:
+	if _is_fully_shown():
+		set_process(false)
+		$"%Continue".visible = true
+	else:
 		_characters_shown += _characters_per_sec * delta
 		_content.visible_characters = round(_characters_shown)
+
+
+func _input(event: InputEvent) -> void:
+	if not visible:
+		set_process_input(false)
+
+	var mouse_event := event as InputEventMouseButton
+	if not mouse_event or not mouse_event.is_pressed():
+		return
+
+	if _is_fully_shown():
+		emit_signal("completed")
 	else:
-		set_process(false)
-		if _characters_shown > 0:
-			emit_signal("completed")
+		_characters_per_sec = 1024
