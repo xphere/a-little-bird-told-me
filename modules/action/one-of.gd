45ac171bbf47fbbe919956b127430ad0c2ddf59f
Berny Cantos <be@rny.cc>
2022-11-17 00:00:41 +0100

    +action: one-of node

---
+++ modules/action/one-of.gd
@@ -0,0 +1,21 @@
+extends Action
+
+var times := 0
+
+
+func execute() -> void:
+	var actions := []
+	for child in get_children():
+		if child is Action:
+			actions.append(child)
+
+	if actions.empty():
+		return
+
+	var limit := actions.size() - (1 if times > 0 else 0)
+	var rand_index := rand_range(0, limit)
+	var action := actions[rand_index] as Action
+
+	times += 1
+	action.raise()
+	yield(RefSignal.as_async(action.execute()), "completed")
