7a198245871245858935ea9b9bcfa68e0455e1fc
Berny Cantos <be@rny.cc>
2022-11-16 22:19:41 +0100

    +action: search recursively for the Game node

--- modules/action/action.gd
+++ modules/action/action.gd
@@ -2,5 +2,14 @@ class_name Action
 extends Node
 
 
+func _enter_tree() -> void:
+	var node : Node = self
+	var iterations := 0
+	while node.owner:
+		iterations += 1
+		node = node.owner
+	owner = node
+
+
 func execute() -> void:
 	pass
