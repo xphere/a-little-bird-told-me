c8570d197f3b05d4261d9be4dcb7d35557d031b2
Berny Cantos <be@rny.cc>
2022-11-21 00:09:09 +0100

    +wait-signal: node defaults to owner

--- modules/action/wait-signal.gd
+++ modules/action/wait-signal.gd
@@ -5,6 +5,6 @@ export var signal_name : String
 
 
 func execute() -> void:
-	var node := get_node(path)
+	var node := owner if path.is_empty() else get_node(path)
 	if node:
 		yield(node, signal_name)
