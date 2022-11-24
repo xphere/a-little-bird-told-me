83486bbfb58ca306c7b6be862b5b6dead0ab9a43
Berny Cantos <be@rny.cc>
2022-11-25 00:19:34 +0100

    !utils: top_owner start on parent when node has no owner

--- modules/nodes/node-utils.gd
+++ modules/nodes/node-utils.gd
@@ -2,7 +2,10 @@ class_name NodeUtils
 
 
 static func top_owner(node: Node) -> Node:
-	var iterations := 0
+	if node.owner == null:
+		node = node.get_parent()
+
 	while node.owner:
 		node = node.owner
+
 	return node
