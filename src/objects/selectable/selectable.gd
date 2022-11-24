028388799a203238a41b92cb42d2aa1263f2f15b
Berny Cantos <be@rny.cc>
2022-11-25 00:20:01 +0100

    !selectable: change owner at enter tree

--- src/objects/selectable/selectable.gd
+++ src/objects/selectable/selectable.gd
@@ -4,6 +4,12 @@ extends Node
 export var id : String
 
 
+func _enter_tree() -> void:
+	if Engine.editor_hint:
+		return
+	owner = NodeUtils.top_owner(self)
+
+
 func select(value: bool) -> void:
 	var shader_material : ShaderMaterial = $Sprite.material
 	shader_material.set_shader_param("enabled", value)
