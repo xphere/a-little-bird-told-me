5689ead067764a33e659241d9d133ad55d069bee
Berny Cantos <be@rny.cc>
2022-11-27 22:11:21 +0100

    +wait: queue

--- modules/system/wait/wait.gd
+++ modules/system/wait/wait.gd
@@ -14,3 +14,50 @@ static func all(elements: Array) -> void:
 
 	for element in elements:
 		yield(resource, "changed")
+
+
+static func queue() -> WaitQueue:
+	return WaitQueue.new()
+
+
+class WaitQueueTurn:
+	extends Resource
+
+	var _turn : GDScriptFunctionState
+
+	func _init(turn: GDScriptFunctionState) -> void:
+		_turn = turn
+
+	func _notification(what: int) -> void:
+		if what == NOTIFICATION_PREDELETE:
+			_turn.call_deferred("resume")
+
+
+class WaitQueue:
+	var _current_turn : GDScriptFunctionState
+
+	func turn() -> Resource:
+		var prev := _current_turn
+		var turn := _wait_to_resume()
+		_current_turn = turn
+
+		var next := _wait_to_resume()
+		if prev:
+			prev.connect("completed", next, "resume")
+		else:
+			next.call_deferred("resume")
+
+		yield(next, "completed")
+		turn.connect("completed", self, "_on_turn", [turn])
+
+		return WaitQueueTurn.new(turn)
+
+	func _wait_to_resume() -> GDScriptFunctionState:
+		return _wait_to_resume_yield()
+
+	func _wait_to_resume_yield() -> void:
+		yield()
+
+	func _on_turn(turn) -> void:
+		if _current_turn == turn:
+			_current_turn = null
