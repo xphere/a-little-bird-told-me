ab7e23fc8df11444e983881f3e91d884e6b9b192
Berny Cantos <be@rny.cc>
2022-12-01 00:57:21 +0100

    !emitter: allow empty signal

--- modules/system/emitter/emitter.gd
+++ modules/system/emitter/emitter.gd
@@ -10,9 +10,14 @@ func emit(value = "") -> void:
 	if not enabled:
 		return
 
-	_signal.emit_signal("signaled", value)
+	if _signal:
+		_signal.emit_signal("signaled", value)
+
 	emit_signal("emitted")
 
 
 func listen(object: Object, method: String, binds := [], flags := 0) -> int:
+	if not _signal:
+		return FAILED
+
 	return _signal.connect("signaled", object, method, binds, flags)
