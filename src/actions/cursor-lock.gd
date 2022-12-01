578f1a19d2c65c03e29769368cc3a204d8b6c6e5
Berny Cantos <be@rny.cc>
2022-12-01 12:44:15 +0100

    +action: unlock parent

--- src/actions/cursor-lock.gd
+++ src/actions/cursor-lock.gd
@@ -1,8 +1,41 @@
 class_name ActionCursorLock, "res://src/actions/cursor-lock.icon.png"
 extends ActionSerial
 
+export var unlock_parent := false
+
+var _lock
+
 
 func execute() -> void:
-	var lock = yield(owner.lock(), "completed")
+	var execution
+
+	if unlock_parent:
+		execution = _unlock_parent_lock()
+	else:
+		execution = _lock()
+
+	yield(execution, "completed")
+
+
+func _lock() -> void:
+	_lock = yield(owner.lock(), "completed")
 	yield(.execute(), "completed")
-	owner.unlock(lock)
+	owner.unlock(_lock)
+
+
+func _unlock_parent_lock() -> void:
+	var parent := _find_parent_lock()
+	if not parent:
+		yield(.execute(), "completed")
+	else:
+		owner.unlock(parent._lock)
+		yield(.execute(), "completed")
+		parent._lock = yield(owner.lock(), "completed")
+
+
+func _find_parent_lock() -> ActionCursorLock:
+	var parent := get_parent()
+	while parent is Node and not (parent as ActionCursorLock):
+		parent = parent.get_parent()
+
+	return parent as ActionCursorLock
